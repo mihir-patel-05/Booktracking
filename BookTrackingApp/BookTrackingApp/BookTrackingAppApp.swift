686de2ead7dcd0a98555ff8b0b459293f731c431
Mihir Patel <87828793+mihir-patel-05@users.noreply.github.com>
2026-03-03 18:29:09 -0500

    created xcode project for this app

---
+++ BookTrackingApp/BookTrackingApp/BookTrackingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookTrackingAppApp.swift
+//  BookTrackingApp
+//
+//  Created by Mihir Patel on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct BookTrackingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
