2f56f3714e50b3272e667c3c172bd97e6c72e99b
Mihir Patel <87828793+mihir-patel-05@users.noreply.github.com>
2026-03-17 23:52:30 -0400

    created a new macos project

---
+++ PageFlowMac/PageFlowMac/Booktracking/Booktracking/BooktrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooktrackingApp.swift
+//  Booktracking
+//
+//  Created by Mihir Patel on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct BooktrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PageFlowMac/PageFlowMac/Booktracking/BooktrackingTests/BooktrackingTests.swift
@@ -0,0 +1,17 @@
+//
+//  BooktrackingTests.swift
+//  BooktrackingTests
+//
+//  Created by Mihir Patel on 3/17/26.
+//
+
+import Testing
+@testable import Booktracking
+
+struct BooktrackingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
